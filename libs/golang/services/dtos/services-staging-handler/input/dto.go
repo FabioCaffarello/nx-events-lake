881cf4436061b494b4afa704c96c9b571e1b140c
FabioCaffarello <fabio.caffarello@gmail.com>
2024-01-07 20:51:00 -0300

    feat: remove output_method to -handler dto

--- libs/golang/services/dtos/services-staging-handler/input/dto.go
+++ libs/golang/services/dtos/services-staging-handler/input/dto.go
@@ -5,8 +5,9 @@ import (
 )
 
 type ProcessingJobDependenciesDTO struct {
-	Service         string                                `json:"service"`
-	Source          string                                `json:"source"`
-    Context         string                                `json:"context"`
-	JobDependencies []sharedDTO.ProcessingJobDependencies `json:"job_dependencies"`
+	Service               string                                `json:"service"`
+	Source                string                                `json:"source"`
+	Context               string                                `json:"context"`
+	ParentJobProcessingId string                                `json:"parent_job_processing_id"`
+	JobDependencies       []sharedDTO.ProcessingJobDependencies `json:"job_dependencies"`
 }
--- libs/golang/services/dtos/services-staging-handler/output/dto.go
+++ libs/golang/services/dtos/services-staging-handler/output/dto.go
@@ -5,9 +5,10 @@ import (
 )
 
 type ProcessingJobDependenciesDTO struct {
-	ID              string                                `json:"id"`
-	Service         string                                `json:"service"`
-	Source          string                                `json:"source"`
-	Context         string                                `json:"context"`
-	JobDependencies []sharedDTO.ProcessingJobDependencies `json:"job_dependencies"`
+	ID                    string                                `json:"id"`
+	Service               string                                `json:"service"`
+	Source                string                                `json:"source"`
+	Context               string                                `json:"context"`
+	ParentJobProcessingId string                                `json:"parent_job_processing_id"`
+	JobDependencies       []sharedDTO.ProcessingJobDependencies `json:"job_dependencies"`
 }
